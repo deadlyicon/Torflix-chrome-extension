05a792a2763735ad69223b9299f1dd6e87067f8d
Jared Grippe <jared@deadlyicon.com>
2015-07-25 17:42:12 -0700

    WIP

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 console.log('background.js')
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+  chrome.app.window.create('app.html', {
     'outerBounds': {
       'width': 400,
       'height': 500
