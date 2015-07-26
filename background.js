3a7c1e82c75c0eedc7626c8c3b5f6023fd116d66
Jared Grippe <jared@deadlyicon.com>
2015-07-25 19:50:35 -0700

    WIP

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+Array.prototype.includes = function(object){
+  return this.indexOf(object) !== -1
+};
+
 Headers = function(headers){
   this.headers = headers;
 };
@@ -6,51 +10,68 @@ Headers.prototype.get = function(name){
   var header;
   for (var i = this.headers.length - 1; i >= 0; i--) {
     header = this.headers[i]
-    if (header.name === name) return header;
+    if (header.name === name) return header.value;
   };
 };
 
 Headers.prototype.log = function(name, value){
-  console.group()
+  console.group();
   this.headers.forEach(function(t){ console.log(t.name+': '+t.value); });
-  console.groupEnd()
+  console.groupEnd();
+  return this;
 };
 
 Headers.prototype.set = function(name, value){
-  var header = this.get(name)
-  if (header){
-    header.value = value
-  }else{
-    this.add(name, value);
+  var header;
+  for (var i = this.headers.length - 1; i >= 0; i--) {
+    header = this.headers[i]
+    if (header.name === name){
+      header.value = value;
+      return this;
+    }
   }
+  this.add(name, value);
+  return this;
 }
+
 Headers.prototype.add = function(name, value){
   this.headers.push({name: name, value: value});
 }
 
 var accessControlRequestHeaders;
 
+MAX_REQEST_IDS_SIZE = 1000
+RequestIds = [];
+pruneRequestIds = function(){
+  RequestIds = RequestIds.slice(0, MAX_REQEST_IDS_SIZE)
+}
+
+AllowedOrigins = [
+  'http://torflix.jaredatron.com'
+]
+
 var requestListener = function(details) {
   var headers = new Headers(details.requestHeaders);
-  console.info('REQUEST HEADERS BEFORE')
-  headers.log()
-  headers.set("Origin","http://evil.com/")
+  var origin = headers.get('Origin');
+  if (AllowedOrigins.includes(origin)){
+    console.log('Torflix Request', details.requestId, details);
+    RequestIds.push(details.requestId)
+  }
+  // headers.set("Origin","http://evil.com/")
   // accessControlRequestHeaders = headers.get("Access-Control-Request-Headers")
-  console.info('REQUEST HEADERS AFTER')
-  headers.log()
-  return {requestHeaders: details.requestHeaders}
+  return {}
 };
 
 
 var responseListener = function(details) {
-  var headers = new Headers(details.responseHeaders);
-  console.info('RESPONSE HEADERS BEFORE')
-  headers.log()
-  headers.set('Access-Control-Allow-Origin', '*')
-  // headers.add('Access-Control-Allow-Headers', accessControlRequestHeaders)
-  headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS');
-  console.info('RESPONSE HEADERS AFTER')
-  headers.log()
+  if (RequestIds.includes(details.requestId)){
+    console.log('Torflix Response', details.requestId, details);
+    var headers = new Headers(details.responseHeaders);
+    headers.set('Access-Control-Allow-Origin', '*')
+    headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS');
+    // headers.add('Access-Control-Allow-Headers', accessControlRequestHeaders)
+  }
+  pruneRequestIds()
   return {responseHeaders: details.responseHeaders};
 };
 
